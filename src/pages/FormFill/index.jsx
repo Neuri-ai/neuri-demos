9afbc7f7ea5328ef35152be0d3a025bd950b543e
Luis Alfredo Reyes <luisalfredoreyes98@gmail.com>
2022-09-22 21:09:46 -0500

    visual fix

--- src/pages/FormFill/index.jsx
+++ src/pages/FormFill/index.jsx
@@ -2,48 +2,41 @@ import './style.css';
 
 const FormFill = () => {
     return (
-        <div>
+        <section className="car">
+            <form className="car-renta">
+                <label>
+                    <p>Pick up location</p>
+                    <input type="text"></input>
+                </label>
 
-            <section>
-        
-                    <section class="car">
-                        <form className="car-renta">
-                            <label>
-                                <p>Pick up location</p>
-                                <input type="text"></input> 
-                            </label>
+                <label>
+                    <p>Drop off location</p>
+                    <input placeholder="Same as pick up location" type="text"></input>
+                </label>
+                <article className="pick">
 
-                            <label>
-                                <p>Drop off location</p>
-                                <input placeholder="Same as pick up location" type="text"></input> 
-                            </label>
-                            <article class="pick">
+                    <div className="off"> <label>
+                        <p>Pick up Date</p>
+                        <input type="date"></input>
+                    </label>
 
-                                <div class="off"> <label>
-                                    <p>Pick up Date</p>
-                                    <input type="date"></input> 
-                                </label>
-
-                                    <label>
-                                        <input type="time"></input> 
-                                    </label>
-                                </div>
-                                <div class="off">
-                                    <label>
-                                        <p>Drop off Date</p>
-                                        <input type="date"></input> 
-                                    </label>
-                                    <label>
-                                        <input type="time"></input> 
-                                    </label>
-                                </div>
-                                <button type="submit">Search</button>
-                            </article>
-                        </form>
-                    </section>
-            </section>
-
-                </div>
-                )
+                        <label>
+                            <input type="time"></input>
+                        </label>
+                    </div>
+                    <div className="off">
+                        <label>
+                            <p>Drop off Date</p>
+                            <input type="date"></input>
+                        </label>
+                        <label>
+                            <input type="time"></input>
+                        </label>
+                    </div>
+                    <button type="submit">Search</button>
+                </article>
+            </form>
+        </section>
+    )
 }
-                export default FormFill
+export default FormFill
--- src/pages/ProductFilter/index.jsx
+++ src/pages/ProductFilter/index.jsx
@@ -1,51 +1,131 @@
+import { useState, useEffect } from 'react';
 import './style.css';
 
 
+const initialSate = [
+	{
+		make: "Gibson",
+		model: "Les Paul",
+		type: "Electric",
+		price: "$3,000",
+		image:
+			"http://www.sweetwater.com/images/items/120/LPST5HTHDCH-medium.jpg?9782bd"
+	},
+	{
+		make: "Gibson",
+		model: "SG",
+		type: "Electric",
+		price: "$1,500",
+		image: "http://www.sweetwater.com/images/items/120/SGSEBCH-medium.jpg?e69cfe"
+	},
+	{
+		make: "Fender",
+		model: "Telecaster",
+		type: "Electric",
+		price: "$2,000",
+		image:
+			"http://www.sweetwater.com/images/items/120/TelePLMPHB-medium.jpg?28e48b"
+	},
+	{
+		make: "Fender",
+		model: "Stratocaster",
+		type: "Electric",
+		price: "$2,000",
+		image:
+			"http://www.sweetwater.com/images/items/120/StratAMM3SB2-medium.jpg?dfd0a9"
+	},
+	{
+		make: "Gretsch",
+		model: "White Falcon",
+		type: "Electric",
+		price: "$5,000",
+		image:
+			"http://www.sweetwater.com/images/items/120/G613655GE-medium.jpg?9bfb0e"
+	},
+	{
+		make: "Paul Reed Smith",
+		model: "Custom 24",
+		type: "Electric",
+		price: "$5,000",
+		image:
+			"http://www.sweetwater.com/images/items/120/HBII10BGWB-medium.jpg?982763"
+	},
+	{
+		make: "Gibson",
+		model: "Hummingbird",
+		type: "Acoustic",
+		price: "$2,500",
+		image: "http://www.sweetwater.com/images/items/120/SSHBHCNP-medium.jpg?11fbea"
+	}
+];
+
+
 const ProductFilter = () => {
-    return(
-    <div class="container">
-	<div class="row" id="search">
-		<form id="search-form" action="" method="POST" enctype="multipart/form-data">
-			<div class="form-group col-xs-9">
-				<input class="form-control" type="text" placeholder="Search" />
+
+	const [data, setData] = useState(initialSate)
+
+
+	useEffect(() => {
+		if(data){
+			data.map((data, key) => {
+				let make = initialSate[key].make
+				let model = initialSate[key].model
+				let type = initialSate[key].type
+				let price = initialSate[key].price
+				let image = initialSate[key].image;
+				var rawPrice = price.replace("$", "")
+				var rawPrice = parseInt(rawPrice.replace(",", ""))
+	
+				console.log(make)
+			})
+		}
+	}, [data])
+
+
+	return (
+		<div className="container product-filter-container">
+			<div className="row" id="search">
+				<form id="search-form" action="" method="POST" encType="multipart/form-data">
+					<div className="form-group col-xs-9">
+						<input className="form-control" type="text" placeholder="Search" />
+					</div>
+					<div className="form-group col-xs-3">
+						<button type="submit" className="btn btn-block btn-primary">Search</button>
+					</div>
+				</form>
 			</div>
-			<div class="form-group col-xs-3">
-				<button type="submit" class="btn btn-block btn-primary">Search</button>
+			<div className="row" id="filter">
+				<form>
+					<div className="form-group col-sm-3 col-xs-6">
+						<select data-filter="make" className="filter-make filter form-control">
+							<option value="">Select Make</option>
+							<option value="">Show All</option>
+						</select>
+					</div>
+					<div className="form-group col-sm-3 col-xs-6">
+						<select data-filter="model" className="filter-model filter form-control">
+							<option value="">Select Model</option>
+							<option value="">Show All</option>
+						</select>
+					</div>
+					<div className="form-group col-sm-3 col-xs-6">
+						<select data-filter="type" className="filter-type filter form-control">
+							<option value="">Select Type</option>
+							<option value="">Show All</option>
+						</select>
+					</div>
+					<div className="form-group col-sm-3 col-xs-6">
+						<select data-filter="price" className="filter-price filter form-control">
+							<option value="">Select Price Range</option>
+							<option value="">Show All</option>
+						</select>
+					</div>
+				</form>
 			</div>
-		</form>
-	</div>  
-	<div class="row" id="filter">
-		<form>
-			<div class="form-group col-sm-3 col-xs-6">
-				<select data-filter="make" class="filter-make filter form-control">
-					<option value="">Select Make</option>
-					<option value="">Show All</option>
-				</select>
+			<div className="row" id="products">
+
 			</div>
-			<div class="form-group col-sm-3 col-xs-6">
-				<select data-filter="model" class="filter-model filter form-control">
-					<option value="">Select Model</option>
-					<option value="">Show All</option>
-				</select>
-			</div>
-			<div class="form-group col-sm-3 col-xs-6">
-				<select data-filter="type" class="filter-type filter form-control">
-					<option value="">Select Type</option>
-					<option value="">Show All</option>
-				</select>
-			</div>
-			<div class="form-group col-sm-3 col-xs-6">
-				<select data-filter="price" class="filter-price filter form-control">
-					<option value="">Select Price Range</option>
-					<option value="">Show All</option>
-				</select>
-			</div>
-		</form>
-	</div>
-	<div class="row" id="products">
-		
-	</div>
-</div>
-    )
+		</div>
+	)
 }
 export default ProductFilter
