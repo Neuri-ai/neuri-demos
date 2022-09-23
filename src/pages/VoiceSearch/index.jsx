c800aca090dfb956e1a3a22dc961f3f43a3b36ec
andrew0815 <israelmart10.go@gmail.com>
2022-09-22 23:34:34 -0500

    barra de busqueda  y chatbot
    añadida barra de busqueda

--- src/pages/VoiceSearch/index.jsx
+++ src/pages/VoiceSearch/index.jsx
@@ -1,8 +1,14 @@
 import './style.css';
 
 const VoiceSearch = () => {
-    return(
-        <h1>Voice search</h1>
-    )
+  return (
+    <div className="container">
+      <form className="bar-search">
+        <input type="search" placeholder="Search..."></input>
+        <button type="submit" className="search-iconbutton">Search</button>
+      </form>
+
+    </div>
+  )
 }
 export default VoiceSearch
