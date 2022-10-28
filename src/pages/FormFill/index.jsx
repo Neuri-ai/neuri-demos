bfb990bcee14e5f52dcd7ce8fe145c2c965f448e
JustoCC <cejajusto08@gmail.com>
2022-10-28 15:51:38 -0500

    uploading minor progress

--- src/pages/FormFill/index.jsx
+++ src/pages/FormFill/index.jsx
@@ -1,42 +1,41 @@
 import './style.css';
 
 const FormFill = () => {
-    return (
-        <section className="car">
-            <form className="car-renta">
-                <label>
-                    <p>Pick up location</p>
-                    <input type="text"></input>
-                </label>
+	return (
+		<section className="DemoFFSection">
+			<form className="DemoFFGrid">
+				<label id='DFG1'>
+					<p>Pick up location</p>
+					<input type="text" className=''></input>
+				</label>
+				<label id='DFG2'>
+					<p>Drop off location</p>
+					<input placeholder="Same as pick up location" type="text"></input>
+				</label>
+				<div id='DFG3'>
+					<div className="off">
+						<label>
+							<p>Pick up Date</p>
+							<input type="date"></input>
+						</label>
 
-                <label>
-                    <p>Drop off location</p>
-                    <input placeholder="Same as pick up location" type="text"></input>
-                </label>
-                <article className="pick">
-
-                    <div className="off"> <label>
-                        <p>Pick up Date</p>
-                        <input type="date"></input>
-                    </label>
-
-                        <label>
-                            <input type="time"></input>
-                        </label>
-                    </div>
-                    <div className="off">
-                        <label>
-                            <p>Drop off Date</p>
-                            <input type="date"></input>
-                        </label>
-                        <label>
-                            <input type="time"></input>
-                        </label>
-                    </div>
-                    <button type="submit">Search</button>
-                </article>
-            </form>
-        </section>
-    )
+						<label>
+							<input type="time"></input>
+						</label>
+					</div>
+					<div className="off">
+						<label>
+							<p>Drop off Date</p>
+							<input type="date"></input>
+						</label>
+						<label>
+							<input type="time"></input>
+						</label>
+					</div>
+				</div>
+				<button type="submit" id='DFFBtn'>Search</button>
+			</form>
+		</section>
+	)
 }
 export default FormFill
