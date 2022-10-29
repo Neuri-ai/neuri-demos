bead90b12bec3dfe685191feb9f991254383c2ac
Luis Alfredo Reyes <luisalfredoreyes98@gmail.com>
2022-10-29 00:06:29 -0500

    realtime speech integrated

--- src/pages/VoiceSearch/index.jsx
+++ src/pages/VoiceSearch/index.jsx
@@ -1,37 +1,50 @@
-import Icons from 'helpers/iconscall';
-import { useEffect } from 'react';
+import Icons from "helpers/iconscall";
+import { useEffect } from "react";
 import { gsap } from "gsap";
-import './style.css';
-import { useState } from 'react';
+import "./style.css";
+import { useState } from "react";
+
+// import RecordRTC from "recordrtc";
+import RecordRTC, {StereoAudioRecorder} from "recordrtc";
 
 const initialtexttest = [
   {
     message: "Hola esto es una prueba",
-    pos: "izq"
+    pos: "izq",
   },
   {
     message: "A que bueno papu",
-    pos: "der"
+    pos: "der",
   },
   {
     message: "Panchito se la come",
-    pos: "izq"
+    pos: "izq",
   },
   {
     message: "Entera y doblada ajajaja",
-    pos: "izq"
+    pos: "izq",
   },
   {
     message: "Santa purisima",
-    pos: "der"
+    pos: "der",
   },
-]
+];
+
+const API_KEY = "1CV7SkQtkxT3gfeI42kTJY7uQlSfW24Kng";
+const SAMPLE_RATE = 16000;
+const LANG = "es-mx";
+const URL = `wss://api.neuri.ai/api/apha/v1/services/audio/realtime?apikey=${API_KEY}&sample_rate=${SAMPLE_RATE}&lang=${LANG}`;
+let recorder;
+let socket;
 
 const VoiceSearch = () => {
-  const [microText, setText] = useState('jj')
+  const [microText, setText] = useState("");
+  const [isRecording, setRecording] = useState(true);
+  const [messages, setMessages] = useState(initialtexttest);
   useEffect(() => {
-    let tl = gsap.timeline()
-    tl.fromTo(".dvscontainer",
+    let tl = gsap.timeline();
+    tl.fromTo(
+      ".dvscontainer",
       {
         opacity: 0,
         scale: 0.9,
@@ -40,8 +53,10 @@ const VoiceSearch = () => {
         opacity: 1,
         scale: 1,
         duration: 1,
-      });
-    tl.fromTo(".chatinput",
+      }
+    );
+    tl.fromTo(
+      ".chatinput",
       {
         opacity: 0,
         y: -100,
@@ -52,27 +67,114 @@ const VoiceSearch = () => {
         scale: 1,
         y: 0,
         duration: 1,
-      });
-  }, [])
+      }
+    );
+  }, []);
+
+  const run = async () => {
+    setRecording(!isRecording);
+
+    if (!isRecording) {
+      // si el microfono esta grabando detener y limpiar la memoria
+      if (recorder) {
+        recorder.pauseRecording();
+        recorder = null;
+      }
+
+      if (socket) {
+        socket.send(JSON.stringify({ terminate_session: true }));
+        socket.close();
+        socket = null;
+      }
+    } else {
+      // connect to the websocket
+      socket = new WebSocket(URL);
+
+      // this function is called when the websocket receives a message
+      socket.onmessage = (event) => {
+        // the first message is the connection state
+        // try parsing the message as JSON if it fails, it's the connection state
+        try {
+          const res = JSON.parse(event.data);
+          if (!res.isFinal) {
+            setText(res.transcription);
+          } else {
+            // cuando este bloque se ejecuta es porque se debe de enviar el mensaje
+            console.log(res.transcription);
+          }
+        } catch (e) {
+          console.log(event.data);
+        }
+      };
+
+      // handle errors
+      socket.onerror = (error) => {
+        console.log(error);
+        socket.close();
+        socket = null;
+      };
+
+      // handle closing the socket
+      socket.onclose = (event) => {
+        console.log(event);
+        socket = null; // clean of memory
+      };
+
+      // this function is called when the websocket is opened
+      socket.onopen = async () => {
+        navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+          recorder = RecordRTC(stream, {
+            type: "audio",
+            recorderType: StereoAudioRecorder,
+            mimeType: "audio/wav",
+            numberOfAudioChannels: 1,
+            desiredSampRate: SAMPLE_RATE,
+            bufferSize: 4096,
+            timeSlice: 250,
+            ondataavailable: (blob) => {
+              const reader = new FileReader();
+
+              reader.onload = () => {
+                // convert the audio to base64
+                const base64data = reader.result;
+                // audio data must be sent as a base64 encoded string
+                if (socket) {
+                  if (socket) {
+                    socket.send(
+                      JSON.stringify({
+                        audio_data: base64data.split("base64,")[1],
+                      })
+                    );
+                  }
+                }
+              };
+              reader.readAsDataURL(blob);
+            },
+          });
+          recorder.startRecording();
+        });
+      };
+    }
+  };
 
   return (
     <div className="dvscontainer">
       <div className="dvschatcontainer">
-        {initialtexttest.map((index, key) => {
+        {messages.map((index, key) => {
           return (
             <div className={`chat ${index.pos}`} key={key}>
               {index.pos === "izq" && <img src={Icons.Neuri}></img>}
               <p className={index.pos}>{index.message}</p>
               {index.pos === "der" && <img src={Icons.User}></img>}
             </div>
-          )
+          );
         })}
       </div>
       <div className="chatinput">
         <p>{microText}</p>
-        <img src={Icons.Microphone} alt="microphone" />
+        <img onClick={() => run()} src={Icons.Microphone} alt="microphone" />
       </div>
     </div>
-  )
-}
-export default VoiceSearch
+  );
+};
+export default VoiceSearch;
