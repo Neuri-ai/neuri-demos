5d0104ed01cb7ed7d7dcee927c57b8707d46ae7c
JustoCC <cejajusto08@gmail.com>
2022-12-15 22:36:05 -0600

    demos v2

--- src/pages/VoiceFormFill/index.jsx
+++ src/pages/VoiceFormFill/index.jsx
@@ -7,7 +7,6 @@ import "./formfill.style.scss";
 import vformcheck from "helpers/VoiceTranscription/VoiceTrans.json"
 import RecordRTC, { StereoAudioRecorder } from "recordrtc";
 
-
 const API_KEY = "sEv5IWnlSCfJ79osIaoLClpZS8nscI2PGw";
 const SAMPLE_RATE = 16000;
 const LANG = "en-us";
@@ -50,6 +49,115 @@ const VoiceFormFill = () => {
     );
   }, []);
 
+  const DateFormatter = ( trans, pos ) => {
+    // preparando el Mes
+    let month = []
+    trans.forEach(elm => {
+      let newMonth = vformcheck['month'].filter(fil => fil['coincidence'].includes(elm))
+      if(Object.keys(newMonth).length > 0) {
+        newMonth.map(obj => {
+          trans.splice(trans.lastIndexOf(elm), 1); month.push(obj['number']) })
+      }
+    })
+
+    // preparando el dia
+    console.log('trans en dia ->', trans)
+    let day = [];
+  
+    trans.forEach( elm => {
+      let dog = vformcheck['date-permited'].filter(fil => fil['coincidence'].includes(elm)).map(obj => {
+        return obj
+      })
+      if (Object.keys(dog).length > 0) {
+        // console.log("dog ->", dog)
+      } else {
+        trans.splice(trans.lastIndexOf(elm), 1)
+      }
+    })
+
+    if(Object.keys(trans).length <= 2){
+      let shifted = trans.join(" ")
+      let newDay = vformcheck['day'].filter(fil => fil['coincidence'].includes(shifted))
+      newDay.map(obj => day.push(obj['number']))
+      let year = new Date().getFullYear()
+      let NewDate = [year, ...month, ...day].join('-')
+      return setFormVal(formVal => ({...formVal, [pos]: NewDate}))
+    }
+
+    let shifted;
+    if ( trans[0] === "twenty" || trans[0] === "twentieth" ) {
+      let checker = [trans[0], trans[1]].join(" ")
+      let whatchdog = vformcheck['day'].filter(fil => fil['coincidence'].includes(checker))
+      whatchdog.map(obj => obj?.number ? shifted = trans.splice(0, 2).join(" ") : null )
+    }
+
+    if ( (trans[0] === "thirty" || trans[0] === "thirtieth") && (trans[1] === "one" || trans[1] === "first") ) { 
+      shifted = trans.splice(0, 2).join(" ") }
+
+    if ( Object.keys(shifted).length <= 0 ) { 
+      shifted = trans.splice(0, 1).join(" ") }
+
+    let newDay = vformcheck['day'].filter(fil => fil['coincidence'].includes(shifted))
+    newDay.map(obj => day.push(obj['number']))
+
+    // preparando el año
+    let year = []
+    if(Object.keys(trans).length === 3){
+      let one = []; let firstpart = trans.shift();
+      let newOne = vformcheck['day'].filter(fil => fil['coincidence'].includes(firstpart))
+      newOne.map(obj => one.push(obj['number']))
+      let two = []
+      let newTwo = vformcheck['day'].filter(fil => fil['coincidence'].includes([...trans].join(' ')))
+      newTwo.map(obj => two.push(obj['number']))
+
+      year = [...one, ...two].join('').split(' ')
+    }
+    if(Object.keys(trans).length > 3){
+      return console.log('working on')
+    }
+
+    //adquiriendo date completo
+    let NewDate = [year, ...month, ...day].flat().join('-')
+    setFormVal(formVal => ({...formVal, [pos]: NewDate}))
+  }
+
+  const TimeFormatter = ( trans, pos ) => {
+    // preparando hora
+    let Hour = []; let timetype;
+    if ( trans.includes('am') ) { timetype = trans.splice(trans.lastIndexOf('am'), 1) }
+    if ( trans.includes('pm') ) { timetype = trans.splice(trans.lastIndexOf('pm'), 1) }
+
+    if ( trans[0] === "twenty" ) {
+      let checker = [trans[0], trans[1]].join(" ")
+      let watchdog = vformcheck['hour'].filter(fil => fil['coincidence'].includes(checker))
+      if ( Object.keys(watchdog).length > 0 ) {
+        watchdog.map(obj => Hour.push(obj["num"]))
+        trans.splice(0, 2)
+      }
+    }
+
+    if ( Object.keys(Hour).length <= 0 ) {
+      let hourshift = trans.shift();
+      vformcheck['hour'].filter(fil => fil['coincidence'].includes(hourshift)).map(obj => Hour.push(obj['num']))
+    }
+
+    if ( Object.keys(trans).length <= 0 ) {
+      let Time = [...Hour, "00"].join(":")
+      return setFormVal(formVal => ({...formVal, [pos]: Time}))
+    }
+
+    if ( Object.keys(trans).length > 2 ) {
+      trans.splice(2, Object.keys(trans).length - 2)
+    }
+
+    // preparando minutos
+    let Minute = []; let min = trans.join(" ")
+    vformcheck['min'].filter(fil => fil['coincidence'].includes(min)).map(obj => Minute.push(obj['num']))
+    let Time = [...Hour, ...Minute].join(":")
+    // console.log(`min: ${min}, Time: ${Time}`)
+    setFormVal(formVal => ({...formVal, [pos]: Time}))
+  }
+
   const DrawerFormatter = ( trans ) => {
     const splitedtrans = trans.split(' ');
     const formatedtrans = splitedtrans.map(obj => {
@@ -63,34 +171,51 @@ const VoiceFormFill = () => {
   }
 
   const InfProces = ( trans ) => {
-// Package from Mexico leaves June 12, 2022 at 1:00 p.m. to Monterrey on July 14, 2022 at 9:00 a.m.
-    console.log(trans)
     const splitedtrans = trans["transcription"].split(' ');
-    console.log(splitedtrans)
 
     if ( splitedtrans.includes('clear') ) return setFormVal(initialFormVal)
-    if ( formVal.input1 === '' && splitedtrans.includes('location')) {
-      let city = splitedtrans[splitedtrans.lastIndexOf('location') + 1];
-      setFormVal(formVal => ({...formVal, input1: city }))
-      /*
-        vformcheck['citys'].filter(filt => filt["coincidence"].includes(city)).map(obj => {
-          setFormVal(formVal => ({...formVal, input1: obj["correct"] })) })
-      */
-    }
-    if ( formVal.date1 === '' && splitedtrans.includes('date')) {
-      let year = new Date().getFullYear()
-      let time = splitedtrans[splitedtrans.lastIndexOf('date') + 1];
-      console.log(time)
-    }
-    if ( formVal.time1 === '' && splitedtrans.includes('time')) return console.log("time 1 esta vacio")
 
-    if ( formVal.input2 === '' && splitedtrans.includes('location') ) {
-      let city = splitedtrans[splitedtrans.lastIndexOf('location') + 1];
-      vformcheck['citys'].filter(filt => filt["coincidence"].includes(city)).map(obj => {
-          setFormVal(formVal => ({...formVal, input2: obj["correct"] })) })
+    // Location 1
+    if ( Object.keys(formVal.input1).length <= 0 && splitedtrans.includes('location')) {
+      splitedtrans.splice(splitedtrans.lastIndexOf('location'), 1)
+      let index = splitedtrans.lastIndexOf('location');
+      delete splitedtrans[index]
+      let location = splitedtrans.filter(elm => elm).join(' ')
+      setFormVal(formVal => ({...formVal, input1: location }))
     }
-    if ( formVal.date2 === '' ) return console.log("")
-    if ( formVal.time2 === '' ) return console.log("time 2 esta vacio")
+
+    // Date 1
+    if ( Object.keys(formVal.date1).length <= 0 && splitedtrans.includes('date')) {
+      splitedtrans.splice(splitedtrans.lastIndexOf('date'), 1)
+      DateFormatter( splitedtrans, "date1" )
+    }
+
+    // Time 1
+    if ( Object.keys(formVal.time1).length <= 0 && splitedtrans.includes('time')) {
+      splitedtrans.splice(splitedtrans.lastIndexOf('time'), 1)
+      TimeFormatter( splitedtrans, "time1" )
+    }
+
+    // Location 2
+    if ( Object.keys(formVal.input2).length <= 0 && splitedtrans.includes('location') ) {
+      splitedtrans.splice(splitedtrans.lastIndexOf('location'), 1)
+      let index = splitedtrans.lastIndexOf('location');
+      delete splitedtrans[index]
+      let location = splitedtrans.filter(elm => elm).join(' ')
+      setFormVal(formVal => ({...formVal, input2: location }))
+    }
+
+    // Date 2
+    if ( Object.keys(formVal.date2).length <= 0 && splitedtrans.includes('date')) {
+      splitedtrans.splice(splitedtrans.lastIndexOf('date'), 1)
+      DateFormatter( splitedtrans, "date2" )
+    }
+
+    // Time 2
+    if ( Object.keys(formVal.time2).length <= 0 && splitedtrans.includes('time')) {
+      splitedtrans.splice(splitedtrans.lastIndexOf('time'), 1)
+      TimeFormatter( splitedtrans, "time2" )
+    }
   }
 
   const run = async () => {
@@ -187,32 +312,32 @@ const VoiceFormFill = () => {
         <form id="DemoFFGrid">
 
           <div id="pickup">
-            <div className="locationdiv">
+            <div className={`locationdiv ${formVal.input1 !== "" ? 'formcolor' : ''}`}>
               <p>Location</p>
               <input type="text" placeholder="Airport" className="" value={formVal.input1} readOnly></input>
             </div>
-            <div className="datediv">
+            <div className={`datediv ${formVal.date1 !== "" ? 'formcolor' : ''}`}>
               <p>Date</p>
               <input type="date" value={formVal.date1} readOnly></input>
             </div>
-            <div className="timediv">
+            <div className={`timediv ${formVal.time1 !== "" ? 'formcolor' : ''}`}>
               <p>Time</p>
-              <input type="time" value="08:00" readOnly></input>
+              <input type="time" value={formVal.time1} readOnly></input>
             </div>
           </div>
 
-          <div id="dropoff">
-            <div className="locationdiv">
+          <div id="dropoff" className={`${(formVal.input1 !== "" && formVal.date1 !== "" && formVal.time1 !== "") ? '' : 'scale'}`}>
+            <div className={`locationdiv ${formVal.input2 !== "" ? 'formcolor' : ''}`}>
                 <p>Location</p>
-                <input placeholder="Same as pick up location" type="text" value={formVal.input2} readOnly></input>
+                <input placeholder="Bus transport" type="text" value={formVal.input2} readOnly></input>
             </div>
-            <div className="datediv">
+            <div className={`datediv ${formVal.date2 !== "" ? 'formcolor' : ''}`}>
               <p>Date</p>
-              <input type="date" value="2022-09-29" readOnly></input>
+              <input type="date" value={formVal.date2} readOnly></input>
             </div>
-            <div className="timediv">
+            <div className={`timediv ${formVal.time2 !== "" ? 'formcolor' : ''}`}>
               <p>Time</p>
-              <input type="time" value="13:00" readOnly></input>
+              <input type="time" value={formVal.time2} readOnly></input>
             </div>
           </div>
 
